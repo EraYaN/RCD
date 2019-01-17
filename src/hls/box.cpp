ee595c05c19a94fdf002be1e1c04b2c763f9359f
Erwin de Haan <EraYaN@users.noreply.github.com>
2019-01-17 10:29:42 +0100

    Updated HLS code

--- src/hls/box.cpp
+++ src/hls/box.cpp
@@ -4,24 +4,25 @@
 
 #define WIDTH 1280
 #define HEIGHT 720
-#define WIDTH_LQ (WIDTH/4) //320
-#define HEIGHT_LQ (HEIGHT/4) //180
-#define FBUF_SIZE (WIDTH_LQ*HEIGHT_LQ)
+#define WIDTH_LQ (WIDTH / 4)   //320
+#define HEIGHT_LQ (HEIGHT / 4) //180
+#define FBUF_SIZE (WIDTH_LQ * HEIGHT_LQ)
 
 #define GR(v) ((v)&0xFF)
-#define GG(v) (((v)&0xFF00)>>8)
-#define GB(v) (((v)&0xFF0000)>>16)
-#define GA(v) (((v)&0xFF000000)>>24)
+#define GG(v) (((v)&0xFF00) >> 8)
+#define GB(v) (((v)&0xFF0000) >> 16)
+#define GA(v) (((v)&0xFF000000) >> 24)
 #define SR(v) ((v)&0xFF)
-#define SG(v) (((v)&0xFF)<<8)
-#define SB(v) (((v)&0xFF)<<16)
-#define SA(v) (((v)&0xFF)<<24)
+#define SG(v) (((v)&0xFF) << 8)
+#define SB(v) (((v)&0xFF) << 16)
+#define SA(v) (((v)&0xFF) << 24)
 
 typedef ap_uint<24> pixel_color;
-typedef ap_axiu<32,1,1,1> pixel_data;
+typedef ap_axiu<32, 1, 1, 1> pixel_data;
 typedef hls::stream<pixel_data> pixel_stream;
 
-typedef struct {
+typedef struct
+{
 	uint32_t color;
 	uint16_t x0;
 	uint16_t y0;
@@ -30,49 +31,54 @@ typedef struct {
 	uint16_t s;
 } rectangle;
 
-
-uint8_t mix_color_R(uint32_t color_top, uint32_t color_bottom, uint8_t alpha){
-	return (uint8_t)(((uint16_t)(255-alpha) * GR(color_bottom) + (uint16_t)(alpha) * GR(color_top)) >> 8);
+uint8_t mix_color_R(uint32_t color_top, uint32_t color_bottom, uint8_t alpha)
+{
+	return (uint8_t)(((uint16_t)(255 - alpha) * GR(color_bottom) + (uint16_t)(alpha)*GR(color_top)) >> 8);
 }
 
-uint8_t mix_color_G(uint32_t color_top, uint32_t color_bottom, uint8_t alpha){
-	return (uint8_t)(((uint16_t)(255-alpha) * GG(color_bottom) + (uint16_t)(alpha) * GG(color_top)) >> 8);
+uint8_t mix_color_G(uint32_t color_top, uint32_t color_bottom, uint8_t alpha)
+{
+	return (uint8_t)(((uint16_t)(255 - alpha) * GG(color_bottom) + (uint16_t)(alpha)*GG(color_top)) >> 8);
 }
 
-uint8_t mix_color_B(uint32_t color_top, uint32_t color_bottom, uint8_t alpha){
-	return (uint8_t)(((uint16_t)(255-alpha) * GB(color_bottom) + (uint16_t)(alpha) * GB(color_top)) >> 8);
+uint8_t mix_color_B(uint32_t color_top, uint32_t color_bottom, uint8_t alpha)
+{
+	return (uint8_t)(((uint16_t)(255 - alpha) * GB(color_bottom) + (uint16_t)(alpha)*GB(color_top)) >> 8);
 }
 
-uint32_t combine_color(uint8_t R, uint8_t G, uint8_t B){
+uint32_t combine_color(uint8_t R, uint8_t G, uint8_t B)
+{
 	return SR(R) | SB(B) | SG(G);
 }
 
-bool is_on_rect(rectangle rect, uint16_t x, uint16_t y){
+bool is_on_rect(rectangle rect, uint16_t x, uint16_t y)
+{
 	return (x >= rect.x0 - rect.s && x < rect.x0 && y >= rect.y0 - rect.s && y <= rect.y1 + rect.s) ||
-			(x > rect.x1 && x <= rect.x1 + rect.s  && y >= rect.y0 - rect.s && y <= rect.y1 + rect.s) ||
-			(x >= rect.x0 && x <= rect.x1 && y > rect.y1 && y <= rect.y1 + rect.s) ||
-			(x >= rect.x0 && x <= rect.x1 && y >= rect.y0 - rect.s && y < rect.y0);
+		   (x > rect.x1 && x <= rect.x1 + rect.s && y >= rect.y0 - rect.s && y <= rect.y1 + rect.s) ||
+		   (x >= rect.x0 && x <= rect.x1 && y > rect.y1 && y <= rect.y1 + rect.s) ||
+		   (x >= rect.x0 && x <= rect.x1 && y >= rect.y0 - rect.s && y < rect.y0);
 }
 
-uint32_t composite_pixel(uint32_t color, uint32_t pixel_in){
+uint32_t composite_pixel(uint32_t color, uint32_t pixel_in)
+{
 	uint8_t alpha = GA(color);
 	uint8_t R = mix_color_R(color, pixel_in, alpha);
 	uint8_t G = mix_color_G(color, pixel_in, alpha);
 	uint8_t B = mix_color_B(color, pixel_in, alpha);
-	return combine_color(R,G,B);
+	return combine_color(R, G, B);
 }
 
 void stream(pixel_stream &src, pixel_stream &dst, pixel_stream &dst_hq, rectangle rect_in, uint8_t idx, uint8_t write_rect)
 {
-#pragma HLS INTERFACE ap_ctrl_none port=return
-#pragma HLS INTERFACE axis port=&src
+#pragma HLS INTERFACE ap_ctrl_none port = return
+#pragma HLS INTERFACE axis port = &src
 
-#pragma HLS INTERFACE axis port=&dst
-#pragma HLS INTERFACE axis port=&dst_hq
-#pragma HLS INTERFACE s_axilite port=rect_in
-#pragma HLS INTERFACE s_axilite port=idx
-#pragma HLS INTERFACE s_axilite port=write_rect
-#pragma HLS PIPELINE II=1
+#pragma HLS INTERFACE axis port = &dst
+#pragma HLS INTERFACE axis port = &dst_hq
+#pragma HLS INTERFACE s_axilite port = rect_in
+#pragma HLS INTERFACE s_axilite port = idx
+#pragma HLS INTERFACE s_axilite port = write_rect
+#pragma HLS PIPELINE II = 1
 
 	// Data to be stored across 'function calls'
 	static uint16_t x = 0;
@@ -82,26 +88,46 @@ void stream(pixel_stream &src, pixel_stream &dst, pixel_stream &dst_hq, rectangl
 
 	//static uint32_t tmp_pixel = 0;
 
-	static rectangle rect1 = {0,0,0,0,0};
-	static rectangle rect2 = {0,0,0,0,0};
-	static rectangle rect3 = {0,0,0,0,0};
-	static rectangle rect4 = {0,0,0,0,0};
-	static rectangle rect5 = {0,0,0,0,0};
-	static rectangle rect6 = {0,0,0,0,0};
-	static rectangle rect7 = {0,0,0,0,0};
-	static rectangle rect8 = {0,0,0,0,0};
+	static rectangle rect1 = {0, 0, 0, 0, 0};
+	static rectangle rect2 = {0, 0, 0, 0, 0};
+	static rectangle rect3 = {0, 0, 0, 0, 0};
+	static rectangle rect4 = {0, 0, 0, 0, 0};
+	static rectangle rect5 = {0, 0, 0, 0, 0};
+	static rectangle rect6 = {0, 0, 0, 0, 0};
+	static rectangle rect7 = {0, 0, 0, 0, 0};
+	static rectangle rect8 = {0, 0, 0, 0, 0};
 
 	//static bool is_second_fb = false;
 	//static pixel_color frame_buffer[FBUF_SIZE*2];
 	//#pragma HLS DEPENDENCE variable=frame_buffer inter false
 
-	if(write_rect != 0){
-		switch(idx){
+	if (write_rect != 0)
+	{
+		switch (idx)
+		{
 		case 0:
 			rect1 = rect_in;
 			break;
 		case 1:
 			rect2 = rect_in;
+			break;
+		case 2:
+			rect3 = rect_in;
+			break;
+		case 3:
+			rect4 = rect_in;
+			break;
+		case 4:
+			rect5 = rect_in;
+			break;
+		case 5:
+			rect6 = rect_in;
+			break;
+		case 6:
+			rect7 = rect_in;
+			break;
+		case 7:
+			rect8 = rect_in;
 			break;
 		}
 	}
@@ -118,7 +144,7 @@ void stream(pixel_stream &src, pixel_stream &dst, pixel_stream &dst_hq, rectangl
 	src >> p_in;
 
 	// Keep track of X and Y
-	if(p_in.user)
+	if (p_in.user)
 		x = y = 0;
 
 	////////////////////////////////
@@ -128,33 +154,50 @@ void stream(pixel_stream &src, pixel_stream &dst, pixel_stream &dst_hq, rectangl
 	pixel_data p_out_resample = p_in;
 
 	// Current (incoming) pixel data
-	bool is_on_rect1 = is_on_rect(rect1,x,y);
-	bool is_on_rect2 = is_on_rect(rect2,x,y);
-	bool is_on_rect3 = is_on_rect(rect3,x,y);
-	bool is_on_rect4 = is_on_rect(rect4,x,y);
-	bool is_on_rect5 = is_on_rect(rect5,x,y);
-	bool is_on_rect6 = is_on_rect(rect6,x,y);
-	bool is_on_rect7 = is_on_rect(rect7,x,y);
-	bool is_on_rect8 = is_on_rect(rect8,x,y);
+	bool is_on_rect1 = is_on_rect(rect1, x, y);
+	bool is_on_rect2 = is_on_rect(rect2, x, y);
+	bool is_on_rect3 = is_on_rect(rect3, x, y);
+	bool is_on_rect4 = is_on_rect(rect4, x, y);
+	bool is_on_rect5 = is_on_rect(rect5, x, y);
+	bool is_on_rect6 = is_on_rect(rect6, x, y);
+	bool is_on_rect7 = is_on_rect(rect7, x, y);
+	bool is_on_rect8 = is_on_rect(rect8, x, y);
 	uint32_t pixel;
 
-	if (is_on_rect1){
-		pixel = composite_pixel(rect1.color,p_in.data);
-	} else if (is_on_rect2){
-		pixel = composite_pixel(rect2.color,p_in.data);
-	} else if (is_on_rect3){
-		pixel = composite_pixel(rect3.color,p_in.data);
-	} else if (is_on_rect4){
-		pixel = composite_pixel(rect4.color,p_in.data);
-	} else if (is_on_rect5){
-		pixel = composite_pixel(rect5.color,p_in.data);
-	} else if (is_on_rect6){
-		pixel = composite_pixel(rect6.color,p_in.data);
-	} else if (is_on_rect7){
-		pixel = composite_pixel(rect7.color,p_in.data);
-	} else if (is_on_rect8){
-		pixel = composite_pixel(rect8.color,p_in.data);
-	} else {
+	if (is_on_rect1)
+	{
+		pixel = composite_pixel(rect1.color, p_in.data);
+	}
+	else if (is_on_rect2)
+	{
+		pixel = composite_pixel(rect2.color, p_in.data);
+	}
+	else if (is_on_rect3)
+	{
+		pixel = composite_pixel(rect3.color, p_in.data);
+	}
+	else if (is_on_rect4)
+	{
+		pixel = composite_pixel(rect4.color, p_in.data);
+	}
+	else if (is_on_rect5)
+	{
+		pixel = composite_pixel(rect5.color, p_in.data);
+	}
+	else if (is_on_rect6)
+	{
+		pixel = composite_pixel(rect6.color, p_in.data);
+	}
+	else if (is_on_rect7)
+	{
+		pixel = composite_pixel(rect7.color, p_in.data);
+	}
+	else if (is_on_rect8)
+	{
+		pixel = composite_pixel(rect8.color, p_in.data);
+	}
+	else
+	{
 		pixel = p_in.data;
 	}
 
@@ -181,10 +224,13 @@ void stream(pixel_stream &src, pixel_stream &dst, pixel_stream &dst_hq, rectangl
 	}*/
 
 	// Increment X and Y counters
-	if(p_in.last) {
+	if (p_in.last)
+	{
 		x = 0;
 		y++;
-	} else {
+	}
+	else
+	{
 		x++;
 	}
 }
